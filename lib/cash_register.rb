18eb9b57d44ccd5b36bc1ef6e2e6fd20ba56fd26
Adam Clemons <adamlouisclemons@gmail.com>
2021-03-01 00:21:35 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -16,7 +16,7 @@ class CashRegister
 
   def apply_discount
     @total *= (1 - @discount)
-    puts "After the discount, the total comes to $#{total}."    
+    "After the discount, the total comes to $#{total}."    
   end
 
 
