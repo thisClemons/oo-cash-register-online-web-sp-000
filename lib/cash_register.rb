35462e6bc29aef595f0016df9c8900556d4fb407
Adam Clemons <adamlouisclemons@gmail.com>
2021-03-01 00:28:32 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -15,8 +15,13 @@ class CashRegister
   end
 
   def apply_discount
-    @total *= (1 - (@discount * 0.01))
-    "After the discount, the total comes to $#{total.round}."
+    if @discount = 0
+      message = "There is no discount to apply."
+    else
+      @total *= (1 - (@discount * 0.01))
+      message = "After the discount, the total comes to $#{total.round}."
+    end
+    message
   end
 
 
