a5ad2f7bb43bc92eaabac98e9c4b46323972a183
Adam Clemons <adamlouisclemons@gmail.com>
2021-03-01 00:17:56 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,10 +1,19 @@
 class CashRegister
 
   attr_accessor :total, :discount
+  attr_reader :items
 
   def initialize(discount = 0)
     @total = 0
     @discount = discount
+  end
+
+  def add_item(title, price, quantity = 1)
+    @total += price
+    @items.push(title)
+
+
+
   end
 
 
